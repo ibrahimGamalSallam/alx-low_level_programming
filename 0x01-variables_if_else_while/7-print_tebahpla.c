f5325100dcac0dcb33ca0b458706faf96f01ef7e
ibrahimGamalSallam <you@example.com>
2023-10-06 16:44:40 -0700

    ttt

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
 
 int main(void)
 {
-	char low;
-
-	for (low = 'z'; low >= 'a'; low--)
-		putchar(low);
-		putchar('\n');
-
-	return (0);
+char letter = 'z';
+while (letter >= 'a')
+{
+putchar(letter);
+letter--;
 }
+putchar('\n'); // Print a newline character
+return (0);
