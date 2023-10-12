2a8f93f997ab62decdf2db742e32da9fcb55e276
ibrahimGamalSallam <you@example.com>
2023-10-12 04:05:46 -0700

    -alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+void print_alphabet(void)
+{
+char letter = 'a';
+
+while (letter <= 'z')
+{
+	_putchar(letter);
+	letter++;
+}
+
+int main(void)
+
+return (0);
+}
