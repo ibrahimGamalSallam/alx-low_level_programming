c0db5e9c0975a0cd3f1c6cb5b5457a6566acbd0c
ibrahimGamalSallam <you@example.com>
2023-10-14 16:47:33 -0700

    last commit ISA

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main - Entry point
-*
-* Return: Always 0 (Success)
-*/
-
-void print_alphabet(void)
-/*
- * d
+ * print_alphabet- Prints the letters from a to z on one line
+ *
  */
+void print_alphabet(void)
 {
 char i;
 for (i = 'a'; i <= 'z'; i++)
