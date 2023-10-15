757c362510e3bd9a60e44b154b983619427651c1
ibrahimGamalSallam <you@example.com>
2023-10-15 14:39:32 -0700

    commit

--- 0x02-functions_nested_loops/2-main.c
+++ 0x02-functions_nested_loops/2-main.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - check the code.
+ * main - check the code for Holberton School students.
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    void _putchar(char c);
-    return (0);
+	print_alphabet_x10();
+	return (0);
 }
