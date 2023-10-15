858fd4b1e836bf7fc7bb2516318db6eb5225cb64
ibrahimGamalSallam <you@example.com>
2023-10-15 15:40:24 -0700

    test

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,14 +8,14 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 	while (i < 10)
- {
-	 char j =  'a';
-	 while (j <= 'z') 
-	 {
-		 putchar(j);
-		 j++; 
-	 }
-	 putchar ('\n');
-	 i++;
+	{
+		char j =  'a';
+		while (j <= 'z');
+		{
+			putchar(j);
+			j++;
+		}
+		putchar ('\n');
+		i++;
  }
 }
