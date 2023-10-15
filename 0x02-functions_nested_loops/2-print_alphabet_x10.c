cf6e2b169a91122ec42f89a21f8b890f94f09d14
ibrahimGamalSallam <you@example.com>
2023-10-14 17:36:54 -0700

    1st commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include "main.h"
+/*
+ *printing alphabet 10 times
+ */
+void print_alphabet_x10(void) {
+    for (int j = 0; j < 10; j++) {
+        print_alphabet();
+    }
+}
