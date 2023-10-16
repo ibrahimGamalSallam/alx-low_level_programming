f820638253fd50aa67d8fb67f28cd05ed26a8821
ibrahimGamalSallam <you@example.com>
2023-10-16 12:13:14 -0700

    commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,8 @@
 #include "main.h"
 /**
- *main - islower.c to check if letter is lower or upper case
- */
-
-/**
+ *_islower - is a function to check if letter is lower or upper case
  * @c: (description of parameter c))*
+ * Return: 1 if lower and return 0 if upper
 */
 int _islower(int c)
 {
