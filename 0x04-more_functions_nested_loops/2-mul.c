1e60324c273a2d7ae83befe4c6d5563b1889d02c
ibrahimGamalSallam <you@example.com>
2023-11-06 09:57:12 -0800

    1

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - a function to multiply numbers
+ * @a: a parmeter to be used
+ * @b: a parameters to be used
+ * Return: always print 0
+ */
+
+int mul(int a, int b)
+{
+	int sum = a * b;
+
+	return (sum);
+}
