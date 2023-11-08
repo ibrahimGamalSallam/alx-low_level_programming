a71d4aea71fc6aecc79f0f68640a3706a1746ce8
ibrahimGamalSallam <you@example.com>
2023-11-08 02:36:21 -0800

    9_6

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	int i = 1;
 
-	for (; i < 100; i++)
+	for (; i <= 100; i++)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 		{
@@ -29,6 +29,6 @@ int main(void)
 			printf("%d ", i);
 		}
 		}
-	printf("Buzz \n");
+	printf(" \n");
 	return (0);
 }
